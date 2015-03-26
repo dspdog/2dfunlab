904f5cdcf776a0eacb126c88e784f04916cb2ee1
mrpawel <pawel745@gmail.com>
2015-03-26 12:32:00 -0400

    up to 1M nodes

--- src/node/NodeWorld.java
+++ src/node/NodeWorld.java
@@ -11,20 +11,22 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * Created by user on 3/24/2015.
  */
 public class NodeWorld {
-    private final static double maxNodeSize = 2;
+    private final static double maxNodeSize = 1;
     private final static MyTree myTree = new MyTree(new Rectangle(0,0,1024,1024), maxNodeSize);
 
     public static CopyOnWriteArrayList<Node> nodes = new CopyOnWriteArrayList<Node>();
     public static int totalNodes = 100000;
 
     public static void buildWorld(){
+        ArrayList<Node> tempNodes = new ArrayList<Node>();
         for(int i=0; i<totalNodes; i++){
-            nodes.add(new Node(Math.random()*512 + 256, Math.random()*512 + 256, maxNodeSize));
+            tempNodes.add(new Node(Math.random()*512 + 256, Math.random()*512 + 256, maxNodeSize));
         }
+        nodes = new CopyOnWriteArrayList<Node>(tempNodes);
     }
 
     public static void drawNodes(Graphics2D g){
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //anti aliasing on
+        //g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //anti aliasing on
         for(Node node : nodes){
             node.draw(g);
         }
