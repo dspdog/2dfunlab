70894f8c70d5cb495f90196dc2d843fab45f2db6
mrpawel <pawel745@gmail.com>
2015-05-03 20:49:26 -0400

    ...

--- src/node/NodeBehaviors.java
+++ src/node/NodeBehaviors.java
@@ -2,8 +2,6 @@ package node;
 
 import com.sun.javafx.geom.Vec2d;
 
-import java.awt.*;
-
 /**
  * Created by user on 4/2/2015.
  */
@@ -22,15 +20,15 @@ public class NodeBehaviors {
         return node.setPos(node.pos.x+(Math.random()-0.5)*scale, node.pos.y+(Math.random()-0.5)*scale);
     }
 
-    public static void findDistancesTo(Node node){
+    public static void pushVoltageFrom(Node startNode){
         for(Node _node : NodeWorld.nodes){_node.distToBase=999999f; _node.visited=false; maxDistance=0;} //clear all distances
-        node.distToBase=0;
-        node.visited=true;
-        node.voltageAccumulator += 100f;
-        findNeighborDistances(node);
+        startNode.distToBase=0;
+        startNode.visited=true;
+        startNode.voltageAccumulator += 100f;
+        pushVoltages(startNode);
     }
 
-    private static void findNeighborDistances(Node node){
+    private static void pushVoltages(Node node){
         float voltageThrottle = 0.1f; // voltage throttle = current?
         float numFreeNeighbors = 0;// node.neighbors.size();
 
@@ -43,10 +41,11 @@ public class NodeBehaviors {
                 maxDistance=Math.max(maxDistance,neighbor.distToBase);
             }
         }
+
         node.visited=true;
         for(Node neighbor : node.neighbors){
             if(!neighbor.visited) {
-                findNeighborDistances(neighbor);
+                pushVoltages(neighbor);
             }
         }
 
